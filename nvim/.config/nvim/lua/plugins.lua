4905c7a65604051a4feee4bfa58e291388e70e59
Michael Kuhardt <dev.mkdd@gmail.com>
2023-05-11 22:56:14 +0200

    [NeoVim] update lualine-conf

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -76,10 +76,22 @@ return require('packer').startup(function(use)
 			  options = {
 				  icons_enabled = true,
 				  theme = 'catppuccin',
+				  section_separators = {
+					  left = '',
+					  right = ''
+				  },
+				  component_separators = {
+					  left = '',
+					  right = ''
+				  },
 				  sections = {
-					  lualine_a = { },
-					  lualine_b = { },
-					  lualine_x = {'filetype'}
+					  lualine_a = {},
+					  lualine_b = {},
+					  lualine_c = {},
+					  lualine_x = {},
+					  lualine_x = {},
+					  lualine_y = {},
+					  lualine_z = {}
 				  }
 			  }
 		  }
