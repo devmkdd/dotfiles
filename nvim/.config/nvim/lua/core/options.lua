c71312e28b8bcee44bf9c06f9afdae2c9bf7f420
Michael Kuhardt <dev.mkdd@gmail.com>
2023-06-16 17:51:05 +0200

    [NeoVim] disabled line for max line length

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -1,18 +1,17 @@
 --[[ options.lua ]]
 
 -- [[ Context ]]
-vim.opt.signcolumn = "yes"		-- str: Show or hide the column for max line lenght
-vim.opt.colorcolumn = '120'		-- str: Show colored column for max line length
-vim.opt.number = true			-- bool: show line numbers
+vim.opt.signcolumn = "no"				-- str: Show or hide the column for max line length
+vim.opt.number = true						-- bool: show line numbers
 vim.opt.relativenumber = true		-- bool: show relative linenumbers
-vim.opt.scrolloff = 4			-- number: minimal number of screenlines that will stay
+vim.opt.scrolloff = 4						-- number: minimal number of screenlines that will stay
 vim.opt.termguicolors = true		-- bool: use terminal colors for more available colors
-vim.opt.showmode = false		-- bool: show or hide mode info in commandline
-vim.opt.cursorline = true		-- bool: show ot hide the current cursor line
+vim.opt.showmode = false				-- bool: show or hide mode info in commandline
+vim.opt.cursorline = true				-- bool: show ot hide the current cursor line
 vim.opt.tabstop = 2
 
 --[[ Theme ]]
-vim.opt.syntax = "on"			-- str: Allow syntax highlighting
+vim.opt.syntax = "on"						-- str: Allow syntax highlighting
 
-vim.opt.splitbelow = true		-- bool: horizontal splits should open below
-vim.opt.splitright = true		-- bool: vertical splits should open at the right side
+vim.opt.splitbelow = true				-- bool: horizontal splits should open below
+vim.opt.splitright = true				-- bool: vertical splits should open at the right side
