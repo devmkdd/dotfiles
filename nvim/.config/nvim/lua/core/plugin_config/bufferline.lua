8a92a136b2a25d2236b75f32f416311fdc9cd3a4
Michael Kuhardt <dev.mkdd@gmail.com>
2023-07-18 22:31:50 +0200

    [NeoVim] Adapt bufferline ui

--- nvim/.config/nvim/lua/core/plugin_config/bufferline.lua
+++ nvim/.config/nvim/lua/core/plugin_config/bufferline.lua
@@ -1,20 +1,38 @@
 
 local macchiato = require("catppuccin.palettes").get_palette "macchiato"
-require('bufferline').setup{
+local bufferline = require('bufferline')
+bufferline.setup{
 				options = {
-								numbers = "ordinal",
-								separator_style = "slant"
-				},
-				-- read highlighting config from catppuccin theme
-				highlights = require("catppuccin.groups.integrations.bufferline").get {
-								styles = { "italic", "bold" },
-								custom = {
-												all = {
-																fill = { bg = macchiato.base },
-												},
-												mocha = {
-																background = { fg = macchiato.text },
-												},
+								always_show_bufferline = true,
+								show_buffer_icons = false,
+								show_buffer_close_icons = false,
+								enforce_regular_tabs = true,
+								separator_style = { '', '' },				-- prevent any separators to appear between the tabs
+								offsets = {
+												{ filetype = "NvimTree",},
 								},
-				},
+								indicator = {
+												style = "none",
+								},
+								modified_icon = "",
+	--			},
+--				highlights = require("catppuccin.groups.integrations.bufferline").get {
+				}
+--				options = {
+--								style_preset = bufferline.style_preset.minimal,
+				--				numbers = "ordinal",
+					--			separator_style = nil--{"",""}
+	--			},
+				-- read highlighting config from catppuccin theme
+			--	highlights = require("catppuccin.groups.integrations.bufferline").get {
+				--				styles = { "italic", "bold" },
+				--				custom = {
+								--				all = {
+									--							fill = { bg = macchiato.base },
+										--		},
+					--							mocha = {
+						--										background = { fg = macchiato.text },
+							--					},
+					--			},
+			--	},
 }
