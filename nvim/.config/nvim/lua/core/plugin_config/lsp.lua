2dfa12f21952a5c254469c6af16a0ebeb28c9a28
Michael Kuhardt <dev.mkdd@gmail.com>
2023-08-22 22:26:07 +0200

    [NeoVim]add config for css lsp server

--- nvim/.config/nvim/lua/core/plugin_config/lsp.lua
+++ nvim/.config/nvim/lua/core/plugin_config/lsp.lua
@@ -11,4 +11,12 @@ end)
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
+require'lspconfig'.cssmodules_ls.setup {
+    on_attach = function (client)
+        -- avoid accepting `definitionProvider` responses from this LSP
+        client.server_capabilities.definitionProvider = false
+        custom_on_attach(client)
+    end,
+}
+
 lsp.setup()
