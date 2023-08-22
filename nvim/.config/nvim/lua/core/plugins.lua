cc39c9ac44e35f83593ca11373d0131b32bef1d1
Michael Kuhardt <712020:60aa88e4-aaf4-42c4-b7a6-cd6311a23464@bots.bitbucket.org>
2023-08-22 21:15:09 +0200

    [Neovim] Add initial lsp-config

--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -75,6 +75,23 @@ return require('packer').startup(function(use)
 					end
 	}
 
+
+	-- LSP
+	use {
+					'VonHeikemen/lsp-zero.nvim',
+					branch = 'v2.x',
+					requires = {
+									-- LSP Support
+									{'neovim/nvim-lspconfig'},             -- Required
+								  {'williamboman/mason.nvim'},           -- Optional
+								  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+								  -- Autocompletion
+									{'hrsh7th/nvim-cmp'},     -- Required
+								  {'hrsh7th/cmp-nvim-lsp'}, -- Required
+									{'L3MON4D3/LuaSnip'},     -- Required
+  }
+}
+
 	-- Treesitter for better syntax highlighting
 	use {
 					'nvim-treesitter/nvim-treesitter',
