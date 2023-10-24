ae0bc9370eb3b3fa2c6281188952d05922d6f339
Michael Kuhardt <dev.mkdd@gmail.com>
2023-10-24 21:39:22 +0200

    [NeoVim] Adapt treesiter to work vor tsx

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -9,6 +9,8 @@ vim.opt.termguicolors = true		-- bool: use terminal colors for more available co
 vim.opt.showmode = false				-- bool: show or hide mode info in commandline
 vim.opt.cursorline = true				-- bool: show ot hide the current cursor line
 vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2					-- number: Number of spaces for each step of (auto)indent.
+vim.opt.autoindent = true
 
 --[[ Theme ]]
 vim.opt.syntax = "on"						-- str: Allow syntax highlighting
