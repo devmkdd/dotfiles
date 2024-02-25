17a98a3a6123c7b120cf006756ace5502a7f7c6e
Michael Kuhardt <dev.mkdd@gmail.com>
2024-02-25 21:12:14 +0100

    [NeoVim] Add preinstalled treesitter parsers

--- nvim/.config/nvim/lua/config/treesitter.lua
+++ nvim/.config/nvim/lua/config/treesitter.lua
@@ -2,8 +2,21 @@ local Module = {}
 
 function Module.setup()
 	require("nvim-treesitter.configs").setup {
+		
 		-- ensure to install all parsers or selected ones
-		ensure_installed = "all",
+		ensure_installed = {
+			"css",
+			"html",
+			"json",
+			"kotlin",
+			"lua",
+			"markdown",
+			"markdown_inline",
+			"swift",
+			"tsx",
+			"typescript",
+			"yaml"
+		},
 
 		-- install languages synchronously to ensure_installed ones
 		sync_install = false,
