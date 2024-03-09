e67ccfcb3efcc2836dffc4669a5fcf5437087a88
Michael Kuhardt <dev.mkdd@gmail.com>
2024-03-09 22:59:28 +0100

    [NeoVim] adapt description fo neotree keymap

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -5,7 +5,7 @@ return {
 	branch = "v3.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-	"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
@@ -13,6 +13,6 @@ return {
 	-- define keymappings
 	keys = {
 		-- toggle the filemanager
-		{ "<leader>fe", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+		{ "<leader>fe", "<cmd>Neotree toggle<cr>", desc = "Fileexplorer (NeoTree)" },
 	},
 }
