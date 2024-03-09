fc6d2f4e3e00f620f2c86613a45b14ee18923c2f
Michael Kuhardt <dev.mkdd@gmail.com>
2024-03-09 13:40:27 +0100

    [NeoVim] adapt treesitter to lazy

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,37 @@
+-- === Definition of Telescope plugin ===
+
+return {
+
+	"nvim-treesitter/nvim-treesitter",
+	-- install languages synchronously to ensure_installed ones
+	sync_install = false,
+
+	build = ":TSUpdate", -- executed on plugin install
+	config = function() 
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+
+			-- ensure to install selected parsers
+			ensure_installed = {
+				"css",
+				"html",
+				"json",
+				"kotlin",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"swift",
+				"tsx",
+				"typescript",
+				"yaml"
+			},
+
+			sync_install = false, -- don't wait for parsers to be installed
+
+			highlight = {
+				enable = true,		-- required or treesitter will not be used
+			},
+		})
+	end
+}
+
