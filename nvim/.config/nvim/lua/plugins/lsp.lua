477086974a2c57683cb470ed626401518f59f957
Michael Kuhardt <dev.mkdd@gmail.com>
2024-03-11 18:15:50 +0100

    [NeoVim] Add mason including lsp config
    Including Lua-Language-Server
    vim defined as global within luarc.json

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,29 @@
+-- ==== LSP configuration using mason
+
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensured_installed = { "lua_ls" }
+			})
+		end
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			vim.keymap.set('n', '<leader>K', vim.lsp.buf.hover, {desc = "hover"})
+			vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {desc = "go to definition"})
+			vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {desc = "code actions"})
+		end
+	}
+}
+
