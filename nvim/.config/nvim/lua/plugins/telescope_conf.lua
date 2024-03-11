4bc88016c5c0ad1bc68fbf405c1b1b2693d566ad
Michael Kuhardt <dev.mkdd@gmail.com>
2024-03-11 18:17:44 +0100

    [NeoVim] add telescope-ui-select
    used for vim.ui.select

--- nvim/.config/nvim/lua/plugins/telescope_conf.lua
+++ nvim/.config/nvim/lua/plugins/telescope_conf.lua
@@ -34,6 +34,22 @@ return {
 		},
 	},
 
+	-- == extension to let vim.ui.select use telescope ui
+	{
+		'nvim-telescope/telescope-ui-select.nvim',
+		config = function()
+			require("telescope").setup {
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown {
+							-- even more opts
+						},
+						require("telescope").load_extension("ui-select")
+					}
+				}
+			}
+		end
+	}
 }
 
 
