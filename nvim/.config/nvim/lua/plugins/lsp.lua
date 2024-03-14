af7cdcdf63fb43dd30d2881d50866a2d2c4c0b40
Michael Kuhardt <dev.mkdd@gmail.com>
2024-03-14 22:22:00 +0100

    [NeoVIm] add marksman and js ls, fixes ensure_installed

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -5,26 +5,31 @@ return {
 		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup()
-		end
+		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensured_installed = { "lua_ls", "tsserver" }
+				ensure_installed = {
+					"jsonls",			-- ls for JSON
+					"lua_ls",			-- ls for lua
+					"marksman",		-- ls for markdown
+					"tsserver",		-- ls for js/ts
+				},
 			})
-		end
+		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({})
+			lspconfig.marksman.setup({{}})
 			lspconfig.tsserver.setup({})
-			vim.keymap.set('n', '<leader>K', vim.lsp.buf.hover, {desc = "hover"})
-			vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {desc = "go to definition"})
-			vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {desc = "code actions"})
-		end
-	}
+			vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, { desc = "hover" })
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "go to definition" })
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "code actions" })
+		end,
+	},
 }
-
