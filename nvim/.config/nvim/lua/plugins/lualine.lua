43c12c8eec5758a155e9d7fd314926fd00c7fd9d
Michael Kuhardt <dev.mkdd@gmail.com>
2024-03-16 23:53:47 +0100

    [NeoVim] remove bufferline and show filename in lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -4,28 +4,27 @@ return {
 	"nvim-lualine/lualine.nvim",
 	config = function()
 		require("lualine").setup({
-
 			options = {
 				icons_enabled = true,
 				always_divide_middle = true,
-    theme = "catppuccin",
-				  section_separators = {
-									left = '',
-									right = ''
-					},
-				  component_separators = {
-									left = '',
-									right = ''
-					},
+				theme = "catppuccin",
+				section_separators = {
+					left = '',
+					right = ''
+				},
+				component_separators = {
+					left = '',
+					right = ''
+				},
 
---    component_separators = '',
-  --  section_separators = { left = '', right = '' },
+				--    component_separators = '',
+				--  section_separators = { left = '', right = '' },
 			},
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
 				lualine_c = {},
-				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_x = { "filename", "fileformat", "filetype" },
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
 			},
@@ -33,7 +32,7 @@ return {
 				lualine_a = {},
 				lualine_b = {},
 				lualine_c = {},
-				lualine_x = { "location" },
+				lualine_x = { "filename"},
 				lualine_y = {},
 				lualine_z = {},
 			},
