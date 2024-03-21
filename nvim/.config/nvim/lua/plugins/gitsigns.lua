53e285334066ba7d7016f3b7ce100846d3c38312
Michael Kuhardt <dev.mkdd@gmail.com>
2024-03-21 22:24:19 +0100

    [NeoVim] additional config for gitsigns

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -4,7 +4,18 @@ return {
 	event = "BufEnter",
 	config = function()
 		require("gitsigns").setup({
-			signcolumn = true,
+			signcolumn = true,	-- show the line indicating changes
+			numhl = false, -- colors the linenumber 
+			linehl = false, -- colors the changed lines
+			signs = {
+				add          = { text = '│' },
+				change       = { text = '│' },
+				delete       = { text = '_' },
+				topdelete    = { text = '‾' },
+				changedelete = { text = '~' },
+				-- don't show signs for untracked lines
+				untracked    = { text = '' },
+  },
 			on_attach = function()
 				vim.wo.signcolumn = "yes"
 			end
@@ -13,6 +24,7 @@ return {
 	keys = {
 		{ "<leader>gbl", "<cmd>Gitsigns blame_line<cr>", desc = "Git: Blame for line" },
 		{ "<leader>gtb", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Git: Toggle Blame line" },
+		{ "<leader>ghl", "<cmd>Gitsigns toggle_linehl<cr>", desc = "Highlight changed lines" },
 	}
 }
 
