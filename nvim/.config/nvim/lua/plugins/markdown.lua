01194be697983c157c53620e228bbc110e15830d
Michael Kuhardt <712020:60aa88e4-aaf4-42c4-b7a6-cd6311a23464@bots.bitbucket.org>
2024-03-21 07:52:39 +0100

    [NeoVim] fixed markdown preview not detecting filetypes

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,15 +1,23 @@
 return {
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	ft = { "markdown", "plantuml" },
-	build = function()
-		vim.fn["mkdp#util#install"]()
-	end,
-	dependencies = {
-		"zhaozg/vim-diagram", -- used for syntax-highlighting in mermaid.js
+	-- plugin for setting recognizing the plantuml mkdp_filetype
+	-- if its not loaded separatly the filetype is not recognized 
+	-- so treesiter etc would not work for markdown or at least plantuml
+	{
 		"aklt/plantuml-syntax",
 	},
-	keys = {
-		vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreview<cr>", { desc = "Markdown Preview" }),
-	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown", "plantuml" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+		-- required to set the recogize and provide command for plantuml files
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown", "plantuml" }
+		end,
+		keys = {
+			vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreview<cr>", { desc = "Markdown Preview" }),
+		},
+	}
 }
