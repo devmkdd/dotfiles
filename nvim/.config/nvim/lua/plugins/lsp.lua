c26de53dfd9d7d5b5d0d3d0f1bcec8c567079a75
Michael Kuhardt <712020:60aa88e4-aaf4-42c4-b7a6-cd6311a23464@bots.bitbucket.org>
2024-03-25 11:03:46 +0100

    [NeoVim] Fix Kotlin language server crash

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -13,6 +13,7 @@ return {
 			require("mason-lspconfig").setup({
 				ensure_installed = {
 					"jsonls",			-- ls for JSON
+					"kotlin_language_server", -- kotlin
 					"lua_ls",			-- ls for lua
 					"marksman",		-- ls for markdown
 					"tsserver",		-- ls for js/ts
@@ -27,6 +28,7 @@ return {
 			-- load lsp capabilities for defined language servers
 			-- have to be passed 
 			local cmp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local utils = require('lspconfig/util')
 
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({
@@ -38,6 +40,17 @@ return {
 			lspconfig.tsserver.setup({
 				capabilities = cmp_capabilities
 			})
+
+			lspconfig.kotlin_language_server.setup({
+				init_options = {
+					-- required for storing data or the server will crash
+					storagePath = utils.path.join(vim.env.XDG_DATA_HOME, "nvim-data")
+				},
+				capabilities = cmp_capabilities,
+				on_attach = on_attach,
+	--			flags = lsp_flags,
+			})
+
 			vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, { desc = "hover" })
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "go to definition" })
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "code actions" })
