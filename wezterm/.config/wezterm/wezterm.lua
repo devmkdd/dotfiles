ef19cb7746be332ecb76ed808b03d2cc2a31d862
Michael Kuhardt <dev.mkdd@gmail.com>
2024-06-08 12:27:32 +0200

    [WezTerm] initial config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,24 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Catppuccin Frappe'
+
+config.font = wezterm.font("Hack Nerd Font", {weight="Regular", stretch="Normal", style="Normal"})
+config.font_size = 14.0
+
+-- ensure alt as composition on mac
+config.use_ime = true
+config.use_dead_keys = true
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = true
+
+
+config.hide_tab_bar_if_only_one_tab = true
+
+-- set decoration to allow resizing but hiding the window bar
+config.window_decorations = "RESIZE"
+
+return config
