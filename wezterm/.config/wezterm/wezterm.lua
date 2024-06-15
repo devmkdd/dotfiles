c9d890ac9e6ffeeea4e6a4c6494ae32cfb036fc6
Michael Kuhardt <dev.mkdd@gmail.com>
2024-06-15 22:04:51 +0200

    [WezTerm] adapt wezterm to ui mode dark/light

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,12 +1,29 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Catppuccin Frappe'
+  else
+    return 'Catppuccin Light'
+  end
+end
+
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 config.default_cursor_style = 'SteadyUnderline'
 
-config.color_scheme = 'Catppuccin Frappe'
+config.color_scheme = scheme_for_appearance(get_appearance())
 
 config.font = wezterm.font("Hack Nerd Font", {weight="Regular", stretch="Normal", style="Normal"})
 config.font_size = 14.0
@@ -17,10 +34,8 @@ config.use_dead_keys = true
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = true
 
-
 config.hide_tab_bar_if_only_one_tab = true
 
 -- set decoration to allow resizing but hiding the window bar
-config.window_decorations = "RESIZE"
 
 return config
