1de4a7420a3e2f0ac3a4dbf6cb4218eaa263b278
Michael Kuhardt <712020:60aa88e4-aaf4-42c4-b7a6-cd6311a23464@bots.bitbucket.org>
2024-08-11 16:38:57 +0200

    [Neovim] don't hide .files in neotree

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -8,6 +8,14 @@ return {
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		opts = {
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+				}
+			}
+
+		}
 	},
 
 	-- define keymappings
