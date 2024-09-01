cfce2b3cd1f54932372d696b95110a6fafb27741
Michael Kuhardt <dev.mkdd@gmail.com>
2024-09-01 21:25:45 +0200

    [NeoVim] Fixed zk-nvim for neovim 0.10.1

--- nvim/.config/nvim/lua/plugins/zk.lua
+++ nvim/.config/nvim/lua/plugins/zk.lua
@@ -3,11 +3,17 @@
 -- -> https://github.com/zk-org/zk
 
 return {
-  "zk-org/zk-nvim",
-  config = function()
+	"zk-org/zk-nvim",
+	commit = "dbf4eeab55b08856c9d6b6722dbff39630bb35eb",
+	config = function()
 		require("zk").setup({
 			picker = "telescope",
 			lsp = {
+
+				config = {
+					cmd  = { "zk", "lsp" },
+					name = "zk",
+				},
 				-- automatically attach buffers in a zk notebkook that match the given filetypes
 				auto_attach = {
 					enabled = true,
@@ -18,8 +24,8 @@ return {
 	end,
 	keys = {
 		-- Search within tags
-		{ "<leader>nt", "<cmd>ZkTags<cr>", desc = "Notes: Search by Tag" },
-		{ "<leader>nn", "<cmd>ZkNotes<cr>", desc = "Notes: Search by Notes" },
-	},
-	ft = "markdown",
-}
+				{ "<leader>nt", "<cmd>ZkTags<cr>", desc = "Notes: Search by Tag" },
+				{ "<leader>nn", "<cmd>ZkNotes<cr>", desc = "Notes: Search by Notes" },
+			},
+			ft = "markdown",
+	}
